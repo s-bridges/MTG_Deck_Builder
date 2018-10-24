adf597601cbad4f69fe8be540e1787551d2ad16f
Seth Bridges <matvmp7@gmail.com>
2018-10-24 16:41:36 -0400

    fixing cards

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,7 @@
 
 window._ = require('lodash');
 window.Popper = require('popper.js').default;
+window.Vue = require('vue');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
