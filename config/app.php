21928103ca6f0506e19a5a3e40c2d1ffb5fcaf74
Seth Bridges <matvmp7@gmail.com>
2018-10-24 18:51:43 -0400

    updated app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'MTG_Deck_Builder'),
+    'name' => env('APP_NAME', 'MTG Deck Builder'),
 
     /*
     |--------------------------------------------------------------------------
