bdc6a874e17391c387cc0527926080047b6b5669
Seth Bridges <matvmp7@gmail.com>
2018-10-24 17:28:54 -0400

    added loading gif

--- resources/js/components/ListCards.vue
+++ resources/js/components/ListCards.vue
@@ -24,8 +24,8 @@
                                 <span v-for="card in filteredCards">   
                                     <div class="col-md-2" style="padding-bottom:1em;">                                 
                                         <lazy-image 
-                                            :src="'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=' + card.multiverse_id + '&type=card'"
-                                            :placeholder='images/loading.gif'  
+                                            v-bind:src="'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=' + card.multiverse_id + '&type=card'"
+                                            v-bind:placeholder='images/loading.gif'  
                                         />
                                         </div>
                                 </span>
