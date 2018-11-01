a4018734208b663975637dc52a5296338ce77a4c
Seth Bridges <matvmp7@gmail.com>
2018-11-01 17:05:31 -0400

    updated contact

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+@push('footer')
+<link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+@endpush
+
+<footer>
+    <div class="footerWrap">
+    <div class="footer">
+      <div class="footerContent">
+      <p>© 2018 MagicDB.us</p>
+      </div>     
+    </div>
+</div>
+</footer>
