d1239568b8b722c4072137f5d331711358207e25
Seth Bridges <matvmp7@gmail.com>
2018-11-06 23:27:33 -0500

    updated seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Seth Bridges',
+            'email' => 'matvmp7@gmail.com',
+            'password' => Hash::make('123456'),
+            'type'=> 'admin'
+        ]);
     }
 }
