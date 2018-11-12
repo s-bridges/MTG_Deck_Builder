116f7c5e869ce87e46c84e0954f52db1b1e22a55
Seth Bridges <matvmp7@gmail.com>
2018-11-12 12:56:36 -0500

    added 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,54 +1,6 @@
 <link href='https://fonts.googleapis.com/css?family=Anton|Passion+One|PT+Sans+Caption' rel='stylesheet' type='text/css'>
-<body>
-<script>function randomNum()
-        {
-            "use strict";
-            return Math.floor(Math.random() * 9)+1;
-        }
-            var loop1,loop2,loop3,time=30, i=0, number, selector3 = document.querySelector('.thirdDigit'), selector2 = document.querySelector('.secondDigit'),
-                selector1 = document.querySelector('.firstDigit');
-            loop3 = setInterval(function()
-            {
-              "use strict";
-                if(i > 40)
-                {
-                    clearInterval(loop3);
-                    selector3.textContent = 4;
-                }else
-                {
-                    selector3.textContent = randomNum();
-                    i++;
-                }
-            }, time);
-            loop2 = setInterval(function()
-            {
-              "use strict";
-                if(i > 80)
-                {
-                    clearInterval(loop2);
-                    selector2.textContent = 0;
-                }else
-                {
-                    selector2.textContent = randomNum();
-                    i++;
-                }
-            }, time);
-            loop1 = setInterval(function()
-            {
-              "use strict";
-                if(i > 100)
-                {
-                    clearInterval(loop1);
-                    selector1.textContent = 4;
-                }else
-                {
-                    selector1.textContent = randomNum();
-                    i++;
-                }
-            }, time);
-            </script>
-
-<style>
+<head>
+    <style>
 {
   font-family: 'PT Sans Caption', sans-serif, 'arial', 'Times New Roman';
 }
@@ -273,6 +225,8 @@
     hr
     { margin: 0px; padding: 0px; border-top: 1px dashed #999; }
     </style>
+</head>
+<body>
         <!-- Error Page -->
             <div class="error">
                 <div class="container-floud">
@@ -288,4 +242,52 @@
                 </div>
             </div>
         <!-- Error Page -->
+<script>
+function randomNum()
+        {
+            "use strict";
+            return Math.floor(Math.random() * 9)+1;
+        }
+            var loop1,loop2,loop3,time=30, i=0, number, selector3 = document.querySelector('.thirdDigit'), selector2 = document.querySelector('.secondDigit'),
+                selector1 = document.querySelector('.firstDigit');
+            loop3 = setInterval(function()
+            {
+              "use strict";
+                if(i > 40)
+                {
+                    clearInterval(loop3);
+                    selector3.textContent = 4;
+                }else
+                {
+                    selector3.textContent = randomNum();
+                    i++;
+                }
+            }, time);
+            loop2 = setInterval(function()
+            {
+              "use strict";
+                if(i > 80)
+                {
+                    clearInterval(loop2);
+                    selector2.textContent = 0;
+                }else
+                {
+                    selector2.textContent = randomNum();
+                    i++;
+                }
+            }, time);
+            loop1 = setInterval(function()
+            {
+              "use strict";
+                if(i > 100)
+                {
+                    clearInterval(loop1);
+                    selector1.textContent = 4;
+                }else
+                {
+                    selector1.textContent = randomNum();
+                    i++;
+                }
+            }, time);
+</script>
 </body>
