85366633498a78ffbb7ec023b0c77c47591542ff
Seth Bridges <matvmp7@gmail.com>
2018-11-13 16:54:05 -0500

    i have no clue what i'm doing

--- database/migrations/2018_11_12_152742_add_cover_to_decks.php
+++ database/migrations/2018_11_12_152742_add_cover_to_decks.php
@@ -16,14 +16,8 @@ class AddCoverToDecks extends Migration
         //
         Schema::table('decks', function($table)
         {
-<<<<<<< HEAD
             $table->integer('cover')->default(195297);
-            $table->string('allow_share')->default('off');
             $table->boolean('deck_of_the_week')->default(0);
-=======
-            $table->integer('cover')->nullable();
-            $table->boolean('allow_share')->default(0);
->>>>>>> parent of 8c29d91... updated admin, dotw, cube card
         });
     }
 
