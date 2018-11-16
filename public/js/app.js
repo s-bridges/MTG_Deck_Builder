0a143024d59014e30e446b2d48b0a9104c9a3678
Seth Bridges <matvmp7@gmail.com>
2018-11-16 16:15:22 -0500

    fixed alex's PR mistakes

--- public/js/app.js
+++ public/js/app.js
@@ -38138,7 +38138,7 @@ var render = function() {
                     _c("div", { staticClass: "row" }, [
                       _c(
                         "div",
-                        { staticClass: "card col-lg-12" },
+                        { staticClass: "card full-width" },
                         [
                           _vm._m(0),
                           _vm._v(" "),
@@ -38228,7 +38228,7 @@ var render = function() {
           _vm._v(" "),
           _c("div", { staticClass: "col-lg-12" }, [
             _c("div", { staticClass: "row" }, [
-              _c("div", { staticClass: "card col-lg-12" }, [
+              _c("div", { staticClass: "card full-width" }, [
                 _c("div", { staticClass: "card-header" }, [
                   _c("h4", { staticClass: "mb-0" }, [
                     _vm._v(_vm._s(_vm.deck.name))
