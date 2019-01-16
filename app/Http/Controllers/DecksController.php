9d4779592087d9c9707a907a70540cfe70a5516f
Seth Bridges <matvmp7@gmail.com>
2019-01-16 14:35:54 -0500

    added sideboard functionality

--- app/Http/Controllers/DecksController.php
+++ app/Http/Controllers/DecksController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Deck;
 use App\User;
 use App\Card;
+use App\Sideboard;
 use Auth;
 use JavaScript;
 
@@ -28,6 +29,9 @@ class DecksController extends Controller
         $deck->description = $this->request->input('description');
         $deck->user_id = Auth::user()->id;
         $cards = $this->request->input('cards');
+        // get sideboard cards, like cards above
+        $sideboard = $this->request->input('sideboard');
+
         // save deck model with data from above
         $deck->save();
         
@@ -45,10 +49,29 @@ class DecksController extends Controller
             return $card;
         });
 
+        // sideboard like cards_array
+        $sideboard_cards_array = [];
+        // loop through all of the cards to set the counts
+        $sideboard = collect($sideboard)->each(function ($sideboard_card, $key) use (&$sideboard_cards_array) {
+            if (array_has($sideboard_cards_array, $sideboard_card['id'])) {
+                // get current card count
+                $sideboard_current_count = (int) $sideboard_cards_array[$sideboard_card['id']]['count'];
+                // take the current count and add a card to it
+                $sideboard_cards_array[$sideboard_card['id']] = ['count' => $sideboard_current_count + 1];
+            } else {
+                $sideboard_cards_array[$sideboard_card['id']] = ['count' => 1];
+            }
+            return $sideboard_card;
+        });
+
+
         // $cards = [3 => ['count' => 4], 6 => ['count' => 4], 8 => ['count' => 4]];
 
         // attach the list of cards to the deck potentially make this a sync?
         $attached = $deck->cards()->attach($cards_array);
+
+        // dupe above for sideboard
+        $attached_sideboard = $deck->sideboard_cards()->attach($sideboard_cards_array);
 
         return response()->json(['status' => true, 'message' => 'Saved Successfully!', 'payload' => $deck->toArray()]);
     }
@@ -63,10 +86,12 @@ class DecksController extends Controller
     public function specificDeck($deck_id) {
         $check = Auth::check();
         $deck_id = (int) $deck_id;
-        // need to fix this so there is an OR where clause...basically, if this is the user's deck, or the deck is allow_share, get me the deck
+        // this query below is perfect     
         $deck = Deck::where('id', $deck_id)
-        ->with('cards')->first();
+        ->with('cards')->with('sideboard_cards')->first();
+
         if($check == true) {
+            // this if below should be put together with the one above using &&
             if ($deck && $deck_id === $deck['id']) {
                 // set editable to false if this isn't the user's deck, otherwise, let them edit their own deck
                 $editable = Auth::user()->id == $deck['user_id'];
@@ -79,10 +104,8 @@ class DecksController extends Controller
             $data = collect(['deck' => $deck]);
                 // only view
                 return view('deck-cards', ['data' => $data]);
-        }
+        }               
         return redirect('/404');
-        // default don't show the page, but if they were allowed to see the deck, the above gets returned in the conditional and this line never gets hit
-        // return view('errors.404');
     }
 
     public function deckOfTheWeek() {
@@ -99,7 +122,7 @@ class DecksController extends Controller
         $deck_id = (int) $deck_request['id'];
         // query for deck by id
         $deck = Deck::where('id', $deck_id)
-        ->with('cards')->first();
+        ->with('cards')->with('sideboard_cards')->first();
         // make sure only deck owner can edit and that a deck existed with that id
         $can_edit = $deck && Auth::user()->id == $deck->user_id ? true: false;
         if ($can_edit) {
@@ -125,14 +148,38 @@ class DecksController extends Controller
                 return $card;
             })->toArray();
 
+            // sideboard like cards_array
+            $sideboard_cards_array = [];
+            // all of the sideboard cards from the request
+            $sideboard = $deck_request['sideboard_cards'];
+            // loop through all of the cards to set the counts
+            $sideboard = collect($sideboard)->each(function ($sideboard_card, $key) use (&$sideboard_cards_array) {
+                if (array_has($sideboard_cards_array, $sideboard_card['id'])) {
+                    // get current card count
+                    $sideboard_current_count = (int) $sideboard_cards_array[$sideboard_card['id']]['pivot']['count'];
+                    // dd($sideboard_current_count);
+                    // take the current count and add a card to it
+                    $sideboard_cards_array[$sideboard_card['id']] = ['count' => $sideboard_current_count + 1];
+                } else {
+                    $sideboard_cards_array[$sideboard_card['id']] = ['count' => $sideboard_card['pivot']['count']];
+                }
+                return $sideboard_card;
+            });
+
+            // dupe above for sideboard, but we use sync since some are being added and others kept
+            $synced_sideboard = $deck->sideboard_cards()->sync($sideboard_cards_array);
+
             // $cards = [3 => ['count' => 4], 6 => ['count' => 4], 8 => ['count' => 4]];
 
             // using cards variable, sync all of the cards to the deck which is essentially removing any relationship with card ids not included in the array, and adding non existant ones
             $synced = $deck->cards()->sync($cards_array);
             
-            if ($synced) {
+            if ($synced || $synced_sideboard) {
                 // messaging that the shit actually happened, for now we can just take a dump
-                dd($synced);
+                return response()->json(['status' => true, 'message' => 'Saved Successfully!', 'payload' => $deck->toArray()]);
+            } else {
+                // we can get more detailed with an error message later on, like actually outputting it here
+                return response()->json(['status' => false, 'message' => 'Sync Failed, please reload the page']);
             }
             
         } else {
