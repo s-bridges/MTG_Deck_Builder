7ba7cb403bfa1bba6bf779c8132df01aefd19414
Seth Bridges <matvmp7@gmail.com>
2019-01-17 16:41:17 -0500

    added sitemap to robots

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="MTG Magic the Gathering Deck Builder. Build and share your MTG Standard Decks." />
     <link rel="icon" href="https://magicdb.us/images/mtgicon_S5w_icon.ico">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
