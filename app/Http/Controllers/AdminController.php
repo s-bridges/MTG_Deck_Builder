0d2ab44dd780f6c28f22ff202179aeb9738ed611
Alex Rindone <arindone@iapp.org>
2019-01-21 11:58:10 -0500

    fixing edit and update for blog posts

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -124,7 +124,17 @@ class AdminController extends Controller
     {
         return view('blog');
     }
-    
+
+    // used for displaying edited blog
+    public function editBlogPost($slug)
+    {
+        $post = Post::where('slug', $slug)->with('user')->first();
+        if ($post) {
+            return view('blog', $post->toArray()); 
+        }
+        return redirect('/404');
+    }
+
     public function saveBlogPost()
     {
         $data = $this->request->all();
@@ -146,6 +156,64 @@ class AdminController extends Controller
             $data['user_id'] = Auth::user()->id;
 
             $saved = Post::create($data);
+            if ($saved) {
+                // messaging that the shit actually happened, for now we can just take a dump
+                return response()->json(['status' => true, 'message' => 'Saved Successfully!', 'payload' => $saved]);
+            } else {
+                // we can get more detailed with an error message later on, like actually outputting it here
+                return response()->json(['status' => false, 'message' => 'Post save failed, please reload the page']);
+            }
+        }
+        // if there were errors from validation, return them
+        return response()->json([
+            'success' => false,
+            'message' => json_decode($validation->errors())
+        ], 422);
+    }
+    public function blogValidation($is_edit = false) {
+        if ($is_edit) {
+            return [
+                'title' => 'required|max:255',
+                'content' => 'required'
+            ];
+        } else {
+            return [
+                'slug' => 'required|unique:posts|max:255',
+                'title' => 'required|unique:posts|max:255',
+                'input_img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'content' => 'required'
+            ];
+        }
+    }
+    public function updateBlogPost()
+    {
+        // dd($this->request);
+        $data = $this->request->all();
+        $image_set = isset($data['input_img']) && $data['input_img'] != "undefined";
+        // make sure title and slug are unique within the DB
+        $validation = Validator::make($data, $this->blogValidation($data['is_edit']));
+        if ($validation->passes()) {
+            // save image 
+            if ($image_set) {
+                $image = $data['input_img'];
+                $image_name = time() . '-' . $image->getClientOriginalName();
+                $destinationPath = public_path('/images');
+                $image->move($destinationPath, $image_name);
+                $data['image_url'] = $image_name;
+            }
+            $updatedData = $data;
+            if (!$image_set) {
+                // unset the image
+                unset($data['image_url']);
+            }
+            if(isset($image_name)) {
+                $updatedData['image_url'] = $image_name;
+            }
+            // update by using the slug since slug is unique
+            $saved = Post::updateOrCreate(
+                ['slug' => $data['slug']],
+                $updatedData
+            );
             if ($saved) {
                 // messaging that the shit actually happened, for now we can just take a dump
                 return response()->json(['status' => true, 'message' => 'Saved Successfully!', 'payload' => $saved]);
