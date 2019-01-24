64184f3994b4caab8fe4dbd15de3dcb3919dff6b
Seth Bridges <matvmp7@gmail.com>
2019-01-24 00:42:27 -0500

    updated blog picture

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function welcome()
+    {
+        // get post from database
+        $post = Post::orderByRaw('RAND()')->first();
+        return view('welcome', ['post' => $post]);
     }
 }
